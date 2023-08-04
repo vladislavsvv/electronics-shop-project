4e634908acbfcc7f7ea5f7a4ecaa92e96b6eaebc
vladislav <vladisla.sv.299563@gmail.com>
2023-08-04 16:21:49 +0600

    add item and test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
+from src.item import Item
+
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+test_item1 = Item('смартфон', 2000, 3)
+
+
+def test_calculate_total_price():
+    assert test_item1.name == 'смартфон'
+    assert test_item1.price == 2000
+    assert test_item1.quantity == 3
+    assert test_item1.calculate_total_price() == 6000
+    test_item1.apply_discount()
+    assert test_item1.price == 1600
